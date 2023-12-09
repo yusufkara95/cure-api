11509d1b674f72d01f36fb19af5e7ffc10e6d2b5
Yusuf Kara <yusufkara@gmx.de>
2023-12-09 12:23:08 +0100

    Default branch

---
+++ src/main/java/de/yusufkara/cureapi/CureApiApplication.java
@@ -0,0 +1,13 @@
+package de.yusufkara.cureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CureApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/yusufkara/cureapi/CureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.yusufkara.cureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
