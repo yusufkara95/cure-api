e94cab6be1e77b2889cda103d99dcfecb5408885
Yusuf Kara <yusufkara@gmx.de>
2023-12-16 15:55:00 +0100

    Add some new attributes for the customer

--- src/main/java/de/yusufkara/cureapi/domain/Customer.java
+++ src/main/java/de/yusufkara/cureapi/domain/Customer.java
@@ -12,14 +12,26 @@ public class Customer{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long customerId;
-    private String firstName, lastName;
+    private String firstName;
+    private String lastName;
+    private String streetAddress;
+    private String houseNumber;
+    private String postalCode;
+    private String city;
+    private String phoneNumber;
+    private String email;
 
     public Customer() {};
 
-    public Customer(String firstName, String lastName) {
-        super();
+    public Customer(String firstName, String lastName, String streetAddress, String houseNumber, String postalCode, String city, String phoneNumber, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.streetAddress = streetAddress;
+        this.houseNumber = houseNumber;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
 
     @JsonIgnore
@@ -57,4 +69,53 @@ public class Customer{
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
